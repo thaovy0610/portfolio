a7979c3f204933909d5e6dfa52ef8e2e9430bc25
thaovy0610 <thaovy0610@gmail.com>
2025-01-22 21:18:30 +0700

    Initial commit for portfolio

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import Slider from './components/Slider.vue'
+
+</script>
+
+<template>
+  <NavBar />
+  <Slider />
+</template>
+
+<style scoped>
+
+</style>
