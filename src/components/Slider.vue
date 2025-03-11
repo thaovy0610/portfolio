5045ba6bc29d3a01d12e007396ef605026c81988
thaovy0610 <thaovy0610@gmail.com>
2025-03-11 16:45:13 +0700

    change font style of character name

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -108,10 +108,18 @@ export default {
 </template>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap');
+
     body {
         background-color: black;
     }
     .transition-transform {
         transition: transform 0.5s ease-in-out;
+    }
+
+    .char-name {
+        font-family: "Irish Grover", system-ui;
+        font-weight: 400;
+        font-style: normal;
     }
 </style>
