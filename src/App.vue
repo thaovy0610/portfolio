b2779f487e6264cc33bc8981acb98f6857855327
thaovy0610 <thaovy0610@gmail.com>
2025-03-18 16:39:07 +0700

    add intro section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
+import NavBar from './components/NavBar.vue'
+import Intro from './components/Intro.vue'
 import Slider from './components/Slider.vue'
 import About from './components/About.vue'
 
 </script>
 
 <template>
-  <About />  
+  <NavBar />
+  <Intro />
   <Slider />
 </template>
 
