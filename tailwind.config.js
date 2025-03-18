bcb9b73f11bc2b554506a70bf6f228cbcc07304f
thaovy0610 <thaovy0610@gmail.com>
2025-03-18 16:58:01 +0700

    Revert "add customized animation and update github action"
    This reverts commit 4818ad45973cbc47237eadc73956cf55c8737f4e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,28 +6,8 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {
-      animation: {
-        'header-reveal': 'header-reveal 2s ease-in-out forwards',
-        'rotate': 'rotate 5s linear forwards',
-        'text-reveal': 'text-reveal 2s ease-in-out forwards',
-      },
-      keyframes: {
-        'header-reveal': {
-          "0%": { transform: 'translateX(-10px)', opacity: 0},
-          "100%": { transform: 'translateY(0)', opacity: 1},
-        },
-        'text-reveal': {
-          "0%": { transform: 'translateY(-10px)', opacity: 0},
-          "100%": { transform: 'translateY(0)', opacity: 1},
-        },
-        'rotate': {
-          to: {
-            transform: 'rotateX(540deg)'
-          }
-        }
-      },
-    },
+    extend: {},
+  },
   plugins: [require('flowbite/plugin')],
-  }
 }
+
