5ee1a7af7ccbbcca565bcf1a427c42936edbe3d9
thaovy0610 <thaovy0610@gmail.com>
2025-03-18 17:07:44 +0700

    add customized annimation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'header-reveal': 'header-reveal 2s ease-in-out forwards',
+        'rotate': 'rotate 5s linear forwards',
+        'text-reveal': 'text-reveal 2s ease-in-out forwards',
+      },
+      keyframes: {
+        'header-reveal': {
+          "0%": { transform: 'translateX(-10px)', opacity: 0},
+          "100%": { transform: 'translateY(0)', opacity: 1},
+        },
+        'text-reveal': {
+          "0%": { transform: 'translateY(-10px)', opacity: 0},
+          "100%": { transform: 'translateY(0)', opacity: 1},
+        },
+        'rotate': {
+          to: {
+            transform: 'rotateX(540deg)'
+          }
+        }
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
